0dbd29263b9fdb62555cbfadf49cf52c40bd7518
Filipe Cruz <suindarakernelchad@gmail.com>
2023-02-03 04:32:54 -0300

    refactor: create helper class avoiding duplicate code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
 import { HttpRequest, HttpResponse } from "../protocols/http"
 import { MissingParamError } from "../errors/missing-param-error"
+import { badRequest } from "../helpers/http-helper"
 
 export class SignUpController {
 
@@ -7,19 +8,13 @@ export class SignUpController {
 
         if(!httpRequest.body.name){
             
-            return {
-                statusCode: 400,
-                body: new MissingParamError('name')
-            }
+            return badRequest(new MissingParamError('name'))
 
         }
 
         if(!httpRequest.body.email){
             
-            return {
-                statusCode: 400,
-                body: new MissingParamError('email')
-            }
+            return badRequest(new MissingParamError('email'))
 
         }
         
