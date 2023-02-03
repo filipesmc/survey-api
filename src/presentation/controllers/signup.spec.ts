e217c5362b5c13d5de88d7703a37f7a22a1306f9
Filipe Cruz <suindarakernelchad@gmail.com>
2023-02-03 03:34:46 -0300

    test: ensure signupcontroller return a 400 error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -19,3 +19,24 @@ describe('SignUp Controller', () => {
     })
         
 })
+
+describe('SignUp Controller', () => {
+    test('Should return 400 if no email is provided', () => {
+        
+        const sut = new SignUpController()
+        
+        const httpRequest = {
+            body: {
+                name: 'Filipe Cruz',
+                password: 'filipe123',
+                passwordConfirmation: 'filipe123'
+            }
+        }
+        
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing parameter: email'))
+    })
+        
+})
+
