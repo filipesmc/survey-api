000a9dd8fd30d8ac1fe65d30059572c391a85984
Filipe Cruz <suindarakernelchad@gmail.com>
2023-02-06 03:41:02 -0300

    refactor: code organization

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -13,17 +13,14 @@ export class SignUpController implements IController{
         try{ 
             const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
             for(const field of requiredFields){
-                if (!httpRequest.body[field]){
-                    return badRequest(new MissingParamError(field))
-                }
+                if (!httpRequest.body[field]){ return badRequest(new MissingParamError(field)) }
             }
             const passwordsMatch = this.passValidator.match(httpRequest.body.password, httpRequest.body.confirmationPassword)
             if(!passwordsMatch) return badRequest(new InvalidParamError('passwordConfirmation'))
             const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
             if(!isValidEmail) return badRequest(new InvalidParamError('email'))
-
-    } catch(error){ 
-        return serverError();
-    }
+        } catch(error){ 
+            return serverError();
+        }
     }
 }
