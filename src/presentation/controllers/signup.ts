34167da798b4b9a6e385fb90ca37432de0704142
Filipe Cruz <suindarakernelchad@gmail.com>
2023-02-06 04:32:33 -0300

    refactor: extract values from httpRequest.body

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,9 +5,11 @@ import { IController } from "../protocols/controller"
 import { IEmailValidator } from "../protocols/email-validator"
 import { InvalidParamError } from "../errors/invalid-param-error"
 import { IPasswordValidator } from "../protocols/password-validator"
+import { IAddAccount } from "../../domain/usecase/add-account"
 
 export class SignUpController implements IController{
-    constructor(private readonly emailValidator: IEmailValidator, private readonly passValidator: IPasswordValidator){ }
+    constructor(private readonly emailValidator: IEmailValidator, private readonly passValidator: IPasswordValidator,
+        private readonly addUserAccountStub: IAddAccount){ }
 
     handle (httpRequest: IHttpRequest): IHttpResponse { 
         try{ 
@@ -15,7 +17,9 @@ export class SignUpController implements IController{
             for(const field of requiredFields){
                 if (!httpRequest.body[field]){ return badRequest(new MissingParamError(field)) }
             }
-            const passwordsMatch = this.passValidator.match(httpRequest.body.password, httpRequest.body.confirmationPassword)
+
+            const { name, email, password, confirmationPassword } = httpRequest.body
+            const passwordsMatch = this.passValidator.match(password, confirmationPassword)
             if(!passwordsMatch) return badRequest(new InvalidParamError('passwordConfirmation'))
             const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
             if(!isValidEmail) return badRequest(new InvalidParamError('email'))
